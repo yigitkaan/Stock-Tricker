31be93c4c7a0ccc1d4ffabfc5deae174c805e7a2
Yiğit Kaan Topcu <65395094+yigitkaan@users.noreply.github.com>
2024-12-24 16:22:32 +0300

    api gücelleme

--- fakeStockAPI.js
+++ fakeStockAPI.js
@@ -1,14 +1,8 @@
 export function getStockData() {
-  const stockDate = new Date();
   return {
     name: "QtechAI",
     sym: "QTA",
     price: (Math.random() * 3).toFixed(2),
-    time:
-      stockDate.getHours() +
-      ":" +
-      stockDate.getMinutes() +
-      ":" +
-      stockDate.getSeconds(),
+    time: new Date().toLocaleTimeString(),
   };
 }
